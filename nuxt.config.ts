9ea88c248408013506044079dcc6512a34a525f6
sfauzi <hello@sfauzi.dev>
2026-04-20 09:34:05 +0700

    feat: :sparkles: first setup

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+    },
+  },
+
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/supabase',
+    '@nuxtjs/tailwindcss'
+  ]
+})
