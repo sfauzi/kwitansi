67e182ec66c5706d8d29ce1a2a86b2cc4f30e7f9
sfauzi <hello@sfauzi.dev>
2026-04-21 09:20:52 +0700

    fix: :bug: mengganti favicon dan setup seo tipis-tipis

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,67 @@ export default defineNuxtConfig({
   supabase: {
     redirect: false // 🔑 MATIkan auto-redirect ke /login
   },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/logo-kwitansi.svg' },
+        { rel: 'apple-touch-icon', href: '/logo-kwitansi.svg' },
+      ],
+      titleTemplate: '%s Kwitansi',
+      meta: [
+        {
+          name: 'description',
+          content: 'Kwitansi - Invoice generator dan pembuat kwitansi online yang mudah digunakan untuk bisnis dan individu.',
+        },
+        {
+          name: 'keywords',
+          content: 'kwitansi, invoice generator, pembuat kwitansi online, template kwitansi, kwitansi digital, kwitansi PDF, kwitansi bisnis, kwitansi pribadi',
+        },
+
+        // Open Graph
+        {
+          property: 'og:title',
+          content: 'Kwitansi - Invoice generator dan pembuat kwitansi online yang mudah digunakan untuk bisnis dan individu. | ',
+        },
+        {
+          property: 'og:description',
+          content: 'Kwitansi - Invoice generator dan pembuat kwitansi online yang mudah digunakan untuk bisnis dan individu.',
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          property: 'og:url',
+          content: 'https://kwitansi-one.vercel.app',
+        },
+        {
+          property: 'og:image',
+          content: 'https://kwitansi-one.vercel.app/og-image.png',
+        },
+
+        // Twitter
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          name: 'twitter:title',
+          content: 'Kwitansi - Invoice generator dan pembuat kwitansi online',
+        },
+        {
+          name: 'twitter:description',
+          content: 'Invoice generator dan pembuat kwitansi online yang mudah digunakan untuk bisnis dan individu.',
+        },
+        {
+          name: 'twitter:image',
+          content: 'https://kwitansi-one.vercel.app/og-image.png',
+        },
+      ],
+    },
+  },
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
